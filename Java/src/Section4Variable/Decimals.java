2485b551b7e1be1abcf0ee4d73f0230b3a8607b3
DESKTOP-G2HQ79N\ASUS <vylinglim2@gmail.com>
2024-07-22 22:37:37 +0800

    decimal

--- Java/src/Section4Variable/Decimals.java
+++ Java/src/Section4Variable/Decimals.java
@@ -1,7 +1,9 @@
 package Section4Variable;
-
+// avoid using int for math calculation
+// always use double for math calculation
 public class Decimals {
     public static void main(String[] args) {
-
+        double percentage = 73.5;
+        System.out.println(percentage + " of percentages are made up");
     }
 }
